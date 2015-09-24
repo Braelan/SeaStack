b87641a54c906670637cbb9661d1401634d0ebd3
App Academy Student <appacademy@Apps-Mac-mini-74.local>
2015-09-24 16:36:46 -0400

    user new redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,20 @@
 class UsersController < ApplicationController
   def new
+    #for redirection in create
+    session[:return_to] ||=request.referer
     @user = User.new
     render :new
   end
 
   def create
-
+    # for redirection back
       @user =User.new(user_params)
 
     if params[:user][:password_confirmation] == user_params[:password] && @user.save
       sign_in(@user)
-      render json: @user
+    #  render json: @user
+      #redirect_to root_url +"/#questions"
+      redirect_to session.delete(:return_to)
     else
       if params[:user][:password_confirmation] != user_params[:password]
          flash.now[:errors] = ["**Passwords do not match**"]
